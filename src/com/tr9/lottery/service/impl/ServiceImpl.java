42f90aa3f08296dad56d64257bb3cc7f02af4145
Sumixe <17681940247@163.com>
2019-07-10 14:48:57 +0800

    第一阶段代码提交1

--- src/com/tr9/lottery/service/impl/ServiceImpl.java
+++ src/com/tr9/lottery/service/impl/ServiceImpl.java
@@ -14,7 +14,6 @@ import java.text.DecimalFormat;
 import java.util.*;
 
 public class ServiceImpl implements Service {
-    private static final Map<String,List> cache=new HashMap<String,List>();
     public List<Bean> list(Integer num) throws Exception {
         HttpUtil httpUtil = new HttpUtil();
         String datastr = httpUtil.getData(num);
@@ -23,7 +22,6 @@ public class ServiceImpl implements Service {
         JSONObject jsonObject = JSONObject.fromObject(datastr);
         ResultObj resultObj =(ResultObj) JSONObject.toBean(jsonObject, ResultObj.class,classMap);
         List<Datas> datas = resultObj.getData();
-        //  System.out.println(datas.toString());
         List<Bean> beans = new ArrayList<Bean>();
         for (Datas data:datas){
             Bean bean = new Bean();
@@ -59,22 +57,20 @@ public class ServiceImpl implements Service {
             }
             beans.add(bean);
         }
-        cache.put("date",beans);
         return beans;
     }
 
     public List<Bean> list(List<Bean> bea,String start, String end) throws Exception{
         //List<Bean> date = cache.get("date");
-        List<Bean> date = this.list(100);
+       // List<Bean> date = this.list(100);
 
         List<Bean> beans = new ArrayList<Bean>();
-        for (Bean bean:date){
+        for (Bean bean:bea){
             String issue = bean.getIssue();
             if (Integer.valueOf(start)>=Integer.valueOf(issue)&&Integer.valueOf(issue)>=Integer.valueOf(end)){
                beans.add(bean);
             }
         }
-        System.out.println("111===="+date.toString());
         return beans;
     }
 
@@ -92,6 +88,7 @@ public class ServiceImpl implements Service {
         System.out.println(substring);
         String[] split = substring.split(",");
         Map<String,Integer> map = new HashMap<String, Integer>();
+        //计算出每个数出现的次数
         for (String ch : split) {
             if (map.containsKey(ch)) {
                 Integer old = map.get(ch);
@@ -104,6 +101,7 @@ public class ServiceImpl implements Service {
         String mapsb = mapstr.substring(1, mapstr.length() - 1);
         String[] mapsp = mapsb.split(",");
         Map<String, String> hashMap = new TreeMap<String, String>();
+        //计算每个概率
         for (String str:mapsp){
             String[] splits = str.split("=");
             String num = splits[0];
@@ -126,6 +124,7 @@ public class ServiceImpl implements Service {
             }
         });
         List<Calc> lastList = new ArrayList<Calc>();
+        //根据概率来提取最大的三位，然后进行判断
         for (int k=datelist.size()-3;k<datelist.size();k++){
             Calc calc = new Calc();
             Map.Entry<String, String> maps = datelist.get(k);
--- src/com/tr9/lottery/util/URLDemo.java
+++ src/com/tr9/lottery/util/URLDemo.java
@@ -29,83 +29,8 @@ import java.util.*;
 public class URLDemo {
     public static void main(String[] args) throws Exception {
 
-        ServiceImpl service = new ServiceImpl();
-        List<Bean> listsd = service.list(10);
-        System.out.println(listsd.toString());
-        List<Bean> beans = service.list(listsd,"07100822", "07100800");
-        int beanSize = beans.size();
-        List<String> list = new ArrayList<String>();
-        for (Bean bean:beans){
-            String sumValue = bean.getSumValue();
-            list.add(sumValue);
-        }
-        String liststr=list.toString();
-        String substring = liststr.substring(1, liststr.length() - 1);
-        System.out.println(substring);
-        String[] split = substring.split(",");
-        Map<String,Integer> map = new HashMap<String, Integer>();
-        for (String ch : split) {
-            if (map.containsKey(ch)) {
-                Integer old = map.get(ch);
-                map.put(ch, old + 1);
-            } else {
-                map.put(ch,1);
-            }
-        }
-        String mapstr=map.toString();
-        String mapsb = mapstr.substring(1, mapstr.length() - 1);
-        String[] mapsp = mapsb.split(",");
-        Map<String, String> hashMap = new TreeMap<String, String>();
-        for (String str:mapsp){
-            String[] splits = str.split("=");
-            String num = splits[0];
-            String frequency = splits[1];
-            Integer integer = Integer.valueOf(frequency);
-            double dou = integer * 1.0 / beanSize;
-            DecimalFormat df = new DecimalFormat("#0.00");
-            String str1 = df.format(dou);
-            hashMap.put(num,str1);
-        }
-
-        //这里将map.entrySet()转换成list
-        List<Map.Entry<String,String>> datelist = new ArrayList<Map.Entry<String,String>>(hashMap.entrySet());
-        //然后通过比较器来实现排序
-        Collections.sort(datelist,new Comparator<Map.Entry<String,String>>() {
-            //升序排序
-            public int compare(Map.Entry<String, String> o1,
-                               Map.Entry<String, String> o2) {
-                return o1.getValue().compareTo(o2.getValue());
-            }
-        });
-        List<Calc> lastList = new ArrayList<Calc>();
-        for (int k=datelist.size()-3;k<datelist.size();k++){
-            Calc calc = new Calc();
-            Map.Entry<String, String> maps = datelist.get(k);
-            String key = maps.getKey();
-            String trim = key.trim();
-            calc.setSumNum(trim);
-            String value = maps.getValue();
-            Double probability = Double.valueOf(value);
-            calc.setProbability(probability);
-            Integer value1 = Integer.valueOf(trim);
-            if (value1%2==0){
-                calc.setSingleOrDouble("双");
-            }else{
-                calc.setSingleOrDouble("单");
-            }
-            if (4<value1&&value1<10){
-                calc.setSize("小");
-
-            }else{
-                calc.setSize("大");
-            }
-
-            lastList.add(calc);
-        }
-
-        System.out.println(lastList.toString());
     }
 
-        //System.out.println(list.toString());
+
 
 }
